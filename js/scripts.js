baf0e750f2348b242720a91124466aae40db6b24
yangx <james891013@outlook.com>
2018-05-29 22:34:00 +0200

    layout completion

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  // Hamburger menu toggle
+  $('.nav-toggle').click(function(){
+    $('.main-nav').toggleClass('is-open');
+    $('.hamburger').toggleClass('is-open');
+  })
+
+
+})
